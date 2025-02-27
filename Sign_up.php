a486a2735e836e4ed1a49624e276823ac1998150
KaloyanGeorgiev20 <kaloyangeorgiev29@itpg-varna.bg>
2025-02-27 21:27:07 +0200

    Add files via upload

--- Sign_up.php
+++ Sign_up.php
@@ -26,7 +26,7 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Нахрани се там - Вкусно!</title>
-    <link rel="stylesheet" href="Sigh_up.css">
+    <link rel="stylesheet" href="Sign_up.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap" rel="stylesheet">
